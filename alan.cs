dc6137f944c90ab3f4027e0f9b7fc7e91329172b
nawangwulan <silviaputri121@gmail.com>
2017-04-09 00:41:17 +0700

    Tester

---
+++ alan.cs
@@ -0,0 +1,17 @@
+MY BIODATA 
+
+Full Name				: Alan Hariyanto
+NickName				: Alan
+Gender					: Male
+Place, Date of Birth	: Serang , October 3th, 1993
+Ages					: 24 years old
+Nationality				: Indonesia
+Height					: 171 cm
+Weight					: 80 kg
+Religions				: Moeslem
+Address					: Bukit Tiara Blok F1/31 Pasirjaya - Cikupa Kab. Tangerang
+Zip Code				: 15710
+Hobby					: Progamming, Badminton, Gaming
+Phone Number			: 081318931839
+Email Address			: apipsisfo@gmail.com
+Job/Work				: Progammer DotNet		
