cde5a1d33ee08b2593dfa180d3ee25d698f6af79
maratunjr <maratunjamilr123@gmail.com>
2017-04-09 22:09:47 +0700

    Maratun Jamil Ramadhani Biodata

---
+++ MaratunJR.cs
@@ -0,0 +1,17 @@
+MY BIODATA 
+
+Full Name				: Mar'atun Jamil Ramadhani
+NickName				: Ani
+Gender					: Female
+Place, Date of Birth	: Purwodadi , February 3th, 1995
+Ages					: 22 years old
+Nationality				: Indonesia
+Height					: 150 cm
+Weight					: 39 kg
+Religions				: Moeslem
+Address					: Kp. Pos Bitung RT/RW 03/04 Desa Kadu Kec.Curug Kab. Tangerang
+Zip Code				: 15710
+Hobby					: Reading
+Phone Number			: 082311319602
+Email Address			: maratunjamilr123@gmail.com
+Job/Work				: Admin Production		
