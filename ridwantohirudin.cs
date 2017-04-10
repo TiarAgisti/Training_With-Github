33e9eca69797fe28af9a9ed826c48d99eca2a920
ridwantohirudin <ridwan.tohirudin@yahoo.com>
2017-04-10 13:02:55 +0700

    ridwan tohirudin biodata

---
+++ ridwantohirudin.cs
@@ -0,0 +1,17 @@
+MY BIODATA 
+
+Full Name				: ridwan tohirudin
+NickName				: ridwan
+Gender					: Male
+Place, Date of Birth	: Tangerang , May 9th, 1998
+Ages					: 18 years old
+Nationality				: Indonesia
+Height					: 172 cm
+Weight					: 62 kg
+Religions				: Moeslem
+Address					: Serdang Wetan - Legok Kab. Tangerang
+Zip Code				: 15710
+Hobby					: Futsal, Gaming
+Phone Number			: 088210742637
+Email Address			: ridwan.tohirudin@yahoo.com
+Job/Work				: staff adm gudang
