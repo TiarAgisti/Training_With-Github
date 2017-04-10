fd2b1de83b825ed4c5aefee46e3b891040f2896e
ngasiman123 <ngasiman21@gmail.com>
2017-04-10 23:19:45 +0700

    Nurahman Biodata

---
+++ Nurahman.cs
@@ -0,0 +1,18 @@
+MY BIODATA 
+
+Full Name				: Nurahman
+NickName				: Rahman
+Gender					: Male
+Place, Date of Birth	: Palembang , October 25th, 1992
+Ages					: 25 years old
+Nationality				: Indonesia
+Height					: 167 cm
+Weight					: 59 kg
+Religions				: Moeslem
+Address					: Kp. Bitung Kab. Tangerang
+Zip Code				: 15710
+Hobby					: futsal and reading
+Phone Number			: 087774052247
+Email Address			: ngasiman21@gmail.com
+Job/Work				: adm Purchase order
+		
