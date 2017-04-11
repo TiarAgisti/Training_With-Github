a54e102e1381dd132b6aec13b41b8aeb879c392c
Your Name <you@example.com>
2017-04-11 15:58:06 +0700

    Setyo Haryanto Bio

---
+++ litle88.cs
@@ -0,0 +1,17 @@
+MY BIODATA 
+
+Full Name				: Setyo Haryanto
+NickName				: Setyo
+Gender					: Male
+Place, Date of Birth	: Ngawi , January 2th, 1995
+Ages					: 22 years old
+Nationality				: Indonesia
+Height					: 170 cm
+Weight					: 57 kg
+Religions				: Moeslem
+Address					: stay here
+Zip Code				: 15710
+Hobby					: Basketball, Gaming
+Phone Number			: 081553395434
+Email Address			: arya.dolphin8@gmail.com
+Job/Work				: Penjual gorengan		
