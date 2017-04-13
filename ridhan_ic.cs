86cf3cde8e29c4d9e1659c6146877b9c15ba62fd
ridhan_ic <icridhan@gmail.com>
2017-04-13 21:22:15 +0700

    Rani Rinjani Ridhan IC Biodata

---
+++ ridhan_ic.cs
@@ -0,0 +1,17 @@
+MY BIODATA 
+
+Full Name				: Rani Rinjani Ridhan IC
+NickName				: Ridhan
+Gender					: female
+Place, Date of Birth			: Sragen, 02 Juli 1997
+Ages					: 19 years old
+Nationality				: Indonesia
+Height					: 160 cm
+Weight					: 52 kg
+Religions				: Moeslem
+Address					: Perum Taman Balarja, Block A4/17, Parahu, Sukamulya Kab. Tangerang
+Zip Code				: 15610
+Hobby					: Playing, adventure :-D
+Phone Number				: 085642441821
+Email Address				: icridhan@gmail.com
+Job/Work				: University Student		
