704d3be92653aa2d2a937893cd42a328d67a05fb
MEGA24 <princesslitle24@gmail.com>
2017-04-15 07:30:11 +0700

    Mega IR Bio

--- mega.cs
+++ mega.cs
@@ -1,14 +1,17 @@
-Full Name		: Mega Indiana Rahayu
-Nick Name		: Mega
-Gender			: Female
-Place, Date Of Birth	: Tangerang, February 24th, 1993
-Ages			: Indonesia
-Height			: 165 cm
-Weight			: 48 kg
-Religions		: Moeslem
-Address			: Stay Here
-Zip code		: 15124
-Hobby			: singing, Badminton
-Phone Number		: 085691465092 ,0895343600753
-Email Adress		: princesslitle24@gmail.com
-Job/Work		: Penjual sandal jepit
+MY BIODATA 
+
+Full Name				: Mega Indiana Rahayu
+NickName				: Mega
+Gender					: Female
+Place, Date of Birth	: Tangerang , February 24th, 1993
+Ages					: 24 years old
+Nationality				: Indonesia
+Height					: 165 cm
+Weight					: 47 kg
+Religions				: Moeslem
+Address					: stay here
+Zip Code				: 15124
+Hobby					: Singing
+Phone Number			: 085691465092
+Email Address			: princesslitle24@gmail.com
+Job/Work				: Penjual sandal jepit		
